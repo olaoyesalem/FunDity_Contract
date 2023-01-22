068fb5be6e5e56d83d2fff41e172d77548ab2755
Olaoye Salem <89555234+olaoyesalem@users.noreply.github.com>
2023-01-22 01:10:39 +0100

    Update PriceConverter.sol

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter{
+
     function getPrice(AggregatorV3Interface priceFeed) internal view returns(uint256){
           (,int256 price,,,)=priceFeed.latestRoundData();
     return uint256(price*1e10);
